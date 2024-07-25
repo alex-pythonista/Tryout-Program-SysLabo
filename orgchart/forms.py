c79e3da4823d61351889c815f283749a7443e381
alex-pythonista <indrajit.alex@gmail.com>
2024-07-25 07:43:13 +0600

    multi-column crispy form field added

--- orgchart/forms.py
+++ orgchart/forms.py
@@ -1,9 +1,30 @@
 from django.forms import ModelForm
+
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.bootstrap import Div, Field
+
 from .models import SysUser
 
 
 class SysUserForm(ModelForm):
 
+    helper = FormHelper()
+    helper.layout = Layout(
+        Div(
+            Field('user_id', wrapper_class='col-md-3'),
+            Field('employee_number', wrapper_class='col-md-3'),
+            Field('department', wrapper_class='col-md-3'),
+            Field('email', wrapper_class='col-md-3'),
+            Field('first_name', wrapper_class='col-md-3'),
+            Field('middle_name', wrapper_class='col-md-3'),
+            Field('last_name', wrapper_class='col-md-3'),
+            Field('gender', wrapper_class='col-md-3'),
+            Field('home_phone', wrapper_class='col-md-3'),
+            Field('mobile_phone', wrapper_class='col-md-3'),
+            Field('title', wrapper_class='col-md-3'),
+            Field('manager', wrapper_class='col-md-3'),
+        css_class='form-row') 
+    )
     class Meta:
         model = SysUser
         fields = (
--- orgchart/urls.py
+++ orgchart/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-employee/', views.add_employee, name='add-employee'),
+    path('about-me/', views.aboutme, name='about-me'),
 ]
